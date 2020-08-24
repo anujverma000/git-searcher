acd76ac659db1737fcd27e1c17ff592d85185c44
Anuj Verma <anujverma000@gmail.com>
2020-08-24 14:45:57 +0400

    fixed layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,19 +1,19 @@
 import Link from "next/link";
 import Head from "next/head";
 import Router from "next/router";
-import NProgress from 'nprogress';
+// import NProgress from 'nprogress';
 
-Router.onRouteChangeStart = () => {
-  NProgress.start();
-}
+// Router.onRouteChangeStart = () => {
+//   NProgress.start();
+// }
 
-Router.onRouteChangeComplete = () => {
-  NProgress.done();
-}
+// Router.onRouteChangeComplete = () => {
+//   NProgress.done();
+// }
 
-Router.onRouteChangeError = () => {
-  NProgress.done();
-}
+// Router.onRouteChangeError = () => {
+//   NProgress.done();
+// }
 
 interface LayoutProps {
     children: React.ReactNode
@@ -24,14 +24,14 @@ interface LayoutProps {
 
 const Layout = ({ children, title, description }: LayoutProps) => (
   <div>
-    <Head>
+    {/* <Head>
       <title>{title || 'Git Searcher'}</title>
       <meta name="description" content={description || 'Best git searcher in the world. '} />
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"/>
     </Head>
     <div className="container">
       {children}
-    </div>
+    </div> */}
   </div>
 );
 
